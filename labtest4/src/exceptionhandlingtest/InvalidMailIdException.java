d8665592dd96b79ada9ae252589f40aa6ef04026
RichaSingh4 <100857253+RichaSingh4@users.noreply.github.com>
2022-12-30 20:09:04 +0530

    Add files via upload

---
+++ labtest4/src/exceptionhandlingtest/InvalidMailIdException.java
@@ -0,0 +1,11 @@
+package exceptionhandlingtest;
+
+//class for invalidmailexception who extend the exception class......
+public class InvalidMailIdException extends Exception{
+	public InvalidMailIdException(String msg) {
+		super(msg);
+	}
+
+	
+
+}
